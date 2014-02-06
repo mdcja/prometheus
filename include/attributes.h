93e23ad2c45b5dea3daaa6a03ad64ed8f821e22c
Julian Martinez del Campo <MartinezDelCampoJ@mymacewan.ca>
2014-02-06 00:16:57 -0700

    Add attributes

---
+++ include/attributes.h
@@ -0,0 +1,122 @@
+/**
+ * @brief Provides data attributes.
+ * @author Julian Martinez del Campo
+ * @license This software is released under the GNU GPL 3 license (or later licenses)
+ *
+ * This file provides data attributes.
+ *
+ * The data attributes are used to manipulate the data stored in the abstract
+ * data types. By changing the attributes, data can be dealt with in a more
+ * controlled way; i.e. printing or copying a string.
+ *
+ * Errors are indicated by the functions return values, error_code will be set
+ * by the functions on error (see error.h). Void functions can also experience
+ * errors, in this case it is necessary to check the value contained in
+ * error_code.
+ *
+ * If NERROR macro is defined, then error messages will not be printed to
+ * stderr.
+ *
+ * If NDEBUG macro is defined then the queue will handle errors without
+ * aborting program execution.
+ */
+#ifndef _ATTRIBUTES_H_
+#define _ATTRIBUTES_H_
+
+#include <stdio.h>
+
+/**
+ * Data attributes.
+ *
+ * This structure holds the attributes that are commonly used when dealing with
+ * data.
+ */
+struct _data_attributes {
+    int (*compare)( const void *, const void * );
+    int (*print)( const void *, FILE * );
+    void * (*copy)( const void * );
+    void (*free)( void * );
+    unsigned long (*hash)( const void * );
+};
+
+typedef struct _data_attributes attr_t;
+
+/**
+ * Initialize attributes.
+ *
+ * This function initializes a data attributes structure. By default the
+ * attributes manipulate pointer values.
+ * 
+ * @post If an error occurs then error_code is set (see error.h).
+ * @post The all data attributes are initialized to default values, and is safe
+ * to use in a data structure.  
+ * @param attr the attributes to initialize.
+ * @return 0 on success, return -1 on error.
+ */
+int attr_init( attr_t * attr );
+
+/**
+ * Set the compare function.
+ *
+ * This function sets the data comparison function. The data comparator takes
+ * two parameters, data A and data B. It then returns -1,0,1 if A is less than,
+ * equal to, or greater than B.
+ *
+ * @post If an error occurs, then error_code is set (see error.h).
+ * @param attr the attributes to set.
+ * @param data_compare the comparator function.
+ * @return 0 on success, return -1 on error.
+ */
+int attr_set_compare( attr_t * attr, int (*data_compare)( const void *, const void * ) );
+
+/**
+ * Set the print function.
+ *
+ * This function sets the data print function. The data print function takes
+ * two parameters, data, and a file pointer to print to. It then returns the
+ * number of characters printed to the stream, or -1 if an error occurred.
+ *
+ * @param attr the attributes to set.
+ * @param data_print the data print function.
+ * @return 0 on success, return -1 on error.
+ */
+int attr_set_print( attr_t * attr, int (*data_print)( const void *, FILE * ) );
+
+/**
+ * Set the copy function.
+ *
+ * This function sets the data copy function. The copy function takes one
+ * parameter, data, and it then returns a pointer to a copy of the data.
+ *
+ * @param attr the attributes to set.
+ * @param data_copy the data copy function.
+ * @return 0 on success, return -1 on error.
+ */
+int attr_set_copy( attr_t * attr, void * (*data_copy)( const void * ) );
+
+/**
+ * Set the free function.
+ *
+ * This function sets the data free function. The free function takes one
+ * parameter, data, and it then deallocates it from memory.
+ *
+ * @param attr the attributes to set.
+ * @param data_free the data deallocation function.
+ * @return 0 on success, return -1 on error.
+ */
+int attr_set_free( attr_t * attr, void (*data_free)( void * ) );
+
+/**
+ * Set the hash function.
+ *
+ * This function sets the data hash function. The hash function takes one
+ * parameter, data, and it then hashes the data and returns the hashed value as
+ * an unsigned long integer.
+ *
+ * @param attr the attributes to set.
+ * @param data_hash the data hash function.
+ * @param 0 on success, return -1 on error.
+ */
+int attr_set_hash( attr_t * attr, unsigned long (*data_hash)( const void * ));
+
+#endif /* _ATTRIBUTES_H_ */
---
+++ src/attributes.c
@@ -0,0 +1,282 @@
+/**
+ * @brief Provides an implementation of data attributes.
+ * @author Julian Martinez del Campo
+ * @license This software is released under the GNU GPL v3 license
+ *
+ * This file provides an implementation of the data attributes. This file also
+ * provides the default data functions.
+ */
+#include <stdlib.h>
+#include <stdio.h>
+#include <assert.h>
+
+#include "attributes.h"
+#include "error.h"
+
+#if 0
+/**
+ * Data attributes.
+ *
+ * This structure holds the attributes that are commonly used when dealing with
+ * data.
+ */
+struct _data_attributes {
+    int (*compare)( const void *, const void * );
+    int (*print)( const void *, FILE * );
+    void * (*copy)( const void * );
+    void (*free)( void * );
+    unsigned long (*hash)( const void * );
+};
+#endif /* 0 */
+
+/**
+ * Default data comparison function.
+ *
+ * This function numerically compares the locations pointed to by data1 and
+ * data2.
+ *
+ * @param data1 an object to compare.
+ * @param data2 another object to compare.
+ * @return -1, 0, 1 if data1 is less than, equal to, or greater than data2.
+ * equal to, or greater than each other.
+ */
+static int default_compare( const void * data1, const void * data2 )
+{
+    if( data1 == data2 )
+    {
+        return 0;
+    }
+    else if( data1 < data2 )
+    {
+        return -1;
+    }
+    else
+    {
+        return 1;
+    }
+}
+
+/**
+ * Default data printing function.
+ *
+ * This function prints the memory location pointed to by data.
+ *
+ * @param fp the file to print to.
+ * @param data the data to print.
+ * @return the number of characters printed to the file, return -1 on error.
+ */
+static int default_print( const void * data, FILE * fp )
+{
+    return fprintf( fp, "%p", data );
+}
+
+/**
+ * Default data copy function.
+ *
+ * This function returns the memory location pointed to by data.
+ *
+ * @post the address of data is returned, not a copy.
+ * @param data the data to copy.
+ * @return the address of data.
+ */
+static void * default_copy( const void * data )
+{
+    return (void *)data;
+}
+
+/**
+ * Default free data function.
+ *
+ * This function does nothing.
+ *
+ * @post the data is still in memory.
+ * @param data to free.
+ */
+static void default_free( void * data )
+{
+    return;
+}
+
+/**
+ * Integer hash key
+ *
+ * CRC variant hash
+ *
+ * @see http://www.cs.hmc.edu/~geoff/classes/hmc.cs070.200101/homework10/hashfuncs.html
+ */
+static unsigned long default_hash( const void * key )
+{
+    unsigned long hash = *(int*)key;
+    unsigned long high_order = 0;
+
+    high_order = hash & 0xf8000000;
+    hash = hash << 5;
+    hash = hash ^ ( high_order >> 27 );
+    hash = hash ^ 1; /* n */
+
+    return hash;
+}
+
+/**
+ * Initialize attributes.
+ *
+ * @post All functions are initialized to the default values.
+ */
+int attr_init( attr_t * attr )
+{
+    if( attr == NULL )
+    {
+        error_code = ERROR_NULL_PARAMETER;
+        error_print( "attr_init" );
+
+        return -1;
+    }
+
+    attr->compare = default_compare;
+    attr->print = default_print;
+    attr->copy = default_copy;
+    attr->free = default_free;
+    attr->hash = default_hash;
+
+    return 0;
+}
+
+/**
+ * Set the compare function.
+ *
+ * @post The data compare function is set.
+ */
+int attr_set_compare( attr_t * attr, int (*data_compare)( const void *, const void *) )
+{
+    if( attr == NULL )
+    {
+        error_code = ERROR_NULL_PARAMETER;
+        error_print( "attr_set_compare" );
+
+        return -1;
+    }
+
+    if( data_compare == NULL )
+    {
+        error_code = ERROR_NULL_PARAMETER;
+        error_print( "attr_set_compare" );
+
+        return -1;
+    }
+
+    attr->compare = data_compare;
+
+    return 0;
+}
+
+/**
+ * Set the print function.
+ *
+ * @post The data print function is set.
+ */
+int attr_set_print( attr_t * attr, int (*data_print)( const void *, FILE * ) )
+{
+    if( attr == NULL )
+    {
+        error_code = ERROR_NULL_PARAMETER;
+        error_print( "attr_set_print" );
+        
+        return -1;
+    }
+
+    if( data_print == NULL )
+    {
+        error_code = ERROR_NULL_PARAMETER;
+        error_print( "attr_set_print" );
+
+        return -1;
+    }
+
+    attr->print = data_print;
+
+    return 0;
+}
+
+/**
+ * Set the copy function.
+ *
+ * @post The data copy function is set.
+ */
+int attr_set_copy( attr_t * attr, void * (*data_copy)( const void * ) )
+{
+    if( attr == NULL )
+    {
+        error_code = ERROR_NULL_PARAMETER;
+        error_print( "attr_set_copy" );
+
+        return -1;
+    }
+
+    if( data_copy == NULL )
+    {
+        error_code = ERROR_NULL_PARAMETER;
+        error_print( "attr_set_copy" );
+
+        return -1;
+    }
+
+    attr->copy = data_copy;
+
+    return 0;
+}
+
+/**
+ * Set the free function.
+ *
+ * @post The data free function is set.
+ */
+int attr_set_free( attr_t * attr, void (*data_free)( void * ) )
+{
+    if( attr == NULL )
+    {
+        error_code = ERROR_NULL_PARAMETER;
+        error_print( "attr_set_free" );
+
+        return -1;
+    }
+
+    if( data_free == NULL )
+    {
+        error_code = ERROR_NULL_PARAMETER;
+        error_print( "attr_set_free" );
+
+        return -1;
+    }
+
+    attr->free = data_free;
+
+    return 0;
+}
+
+/**
+ * Set the hash function.
+ *
+ * @post The data hash function is set.
+ */
+int attr_set_hash( attr_t * attr, unsigned long (*data_hash)(const void *) )
+{
+    if( attr == NULL )
+    {
+        error_code = ERROR_NULL_PARAMETER;
+        error_print( "attr_set_hash" );
+
+        return -1;
+    }
+
+    if( data_hash == NULL )
+    {
+        error_code = ERROR_NULL_PARAMETER;
+        error_print( "attr_set_hash" );
+
+        return -1;
+    }
+
+    attr->hash = data_hash;
+
+    return 0;
+}
