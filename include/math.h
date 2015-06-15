00baf4a5815e5d302f482b6f07763b2c34ff885e
Julian Martinez del Campo <MartinezDelCampoJ@mymacewan.ca>
2015-06-14 22:32:48 -0600

    Change documentation style

--- include/math.h
+++ include/math.h
@@ -1,35 +1,18 @@
-/**
- * This file provides misc math functions.
+/* Author: Julian Martinez del Campo
  *
- * \brief provides generic math functions.
- * \author Julian Martinez del Campo
- * \license GNU Lesser General Public License (LGPL) https://www.gnu.org/copyleft/lesser.html
- *
- * This file provides math functions that are often used and/or not shipped
- * with the standard math libraries available.
+ * This file provides some commonly used math routines.
  */
 #ifndef _MATH_H_
 #define _MATH_H_
 
-/**
- * Generate a prime number.
- *
- * This function generates a prime number that is just below the desired limit.
- *
- * \param limit the limit at which prime numbers will not be generated. Limit
- *  must be greater than 2.  
- * \return the last prime number under limit.
+/* Generate a prime number. This function returns the closest prime number
+ * less than the the limit specified, or else it returns 0 if unsuccessful or 
+ * on error.
  */
 unsigned long generate_prime( unsigned int limit );
 
-/**
- * Generate a random number.
- *
- * This function generates a random number between min and max.
- *
- * \param min the minimum number value.
- * \param max the maximum number value.
- * \return a random number.
+/* Generate a random number. This function returns a random number between min
+ * and max.
  */
 int random_number( int min, int max );
 
